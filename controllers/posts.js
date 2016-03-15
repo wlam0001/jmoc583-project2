bb2b0750453bd0cbd686ad23d56b4a7327792f1b
Emily Lam <wlam0001@live.unc.edu>
2016-03-14 22:12:34 -0400

    Basic form`

---
+++ controllers/posts.js
@@ -0,0 +1,68 @@
+var db = require('../config/db');
+
+exports.list = function(req, res){
+  var collection = db.get().collection('posts');
+
+  collection.find({}).toArray(function(err, results){
+    res.render('posts/list', {posts:results});
+  });
+};
+
+exports.show = function(req, res) {
+    var collection = db.get().collection('posts');
+
+    collection.find({"title": req.params.id}).limit(1).toArray(function(err, results) {
+        res.render('posts/show', {post: results[0]});
+    });
+};
+
+exports.form = function (req, res){
+  res.render('posts/form');
+};
+
+exports.update = function (req, res){
+  var collection = db.get().collection('posts');
+
+  collection.updateOne(
+        {title: req.params.id},
+        {
+            $set: {
+              title: req.body.title,
+              author: req.body.author,
+              category: req.body.category,
+              image: req.body.image,
+              date: req.body.date,
+              content: req.body.content
+            }
+        }
+    );
+
+  res.redirect('/posts');
+};
+
+exports.create = function (req, res){
+  var collection = db.get().collection('posts');
+  collection.insert({
+    title: req.body.title,
+    author: req.body.author,
+    category: req.body.category,
+    image: req.body.image,
+    date: req.body.date,
+    content: req.body.content
+  }, function (err, doc) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        }
+    });
+  res.redirect('/posts');
+};
+
+exports.remove = function(req, res){
+  var collection = db.get().collection('posts');
+
+  collection.remove({
+    title: req.params.id
+  });
+
+  return res.redirect('/posts');
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var db = require('./config/db');
+var post = require('./controllers/posts');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set('view engine', 'ejs');
+
+app.get('/posts', post.list);
+
+app.get('/posts/new', post.form);
+app.post('/posts', post.create);
+
+app.post('/posts/:id', post.update);
+app.get('/posts/:id', post.show);
+
+app.get('/posts/delete/:id', post.remove);
+
+db.connect('mongodb://localhost:27017/content', function(){
+  console.log('MongoDB started...');
+  app.listen(3000,function(){
+    console.log("Express started");
+  });
+});
